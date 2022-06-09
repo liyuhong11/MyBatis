d0083fb270c33cadc4f62d6044f47c495b4a2b5d
lyhong <lyhong@136.com>
2022-06-09 23:59:19 +0800

    mybatis second commit

---
+++ MyBatis_MBG/src/main/java/com/lyh/mybatis/git/GitTest.java
@@ -0,0 +1,12 @@
+package com.lyh.mybatis.git;
+
+/**
+ * @Description:
+ * @Author: lyhstart
+ * @Date: 2022/06/09/23:58
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello git!!!");
+    }
+}
