a1a5b625d9a61cbfd962e49fe97330b4566add1b
lyhong <lyhong@136.com>
2022-06-10 00:24:13 +0800

    ssh commit

--- MyBatis_MBG/src/main/java/com/lyh/mybatis/git/GitTest.java
+++ MyBatis_MBG/src/main/java/com/lyh/mybatis/git/GitTest.java
@@ -11,5 +11,6 @@ public class GitTest {
         System.out.println("hello git!!!");
         System.out.println("hello master!!!");
         System.out.println("hello hot fix!!!");
+        System.out.println("hello ssh!!!");
     }
 }
